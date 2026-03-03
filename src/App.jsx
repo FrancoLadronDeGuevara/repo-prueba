ee5bd85ad2f92e41e2b63a3ad2697a5ff2aef814
FrancoLadronDeGuevara <francoguevara93@gmail.com>
2026-03-02 21:50:09 -0300

    Simulation

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
 import AboutPage from "./pages/AboutPage";
+import Layout from "./components/Layout";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="*" element={<NotFoundPage />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
     </Routes>
   );
 };
